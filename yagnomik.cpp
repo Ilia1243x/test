68a9df5a8a52830050b43b46798bf5f270c5b755
Ilia1243x <ilia.indebaum@gmail.com>
2024-01-09 17:50:57 +0300

    написал сложение

--- yagnomik.cpp
+++ yagnomik.cpp
@@ -1,7 +1,15 @@
 //
 // Created by ilya1243 on 09.01.24.
 //
+
+enum ERRORS {
+    BAD_RESULT
+};
+
 int BIMBIMBAMBAM(int BIMBIM, int BAMBAM) {
-    int JOPA = BIMBIM + BAMBAM;
-    return JOPA;
+    long long result = static_cast<long long>(BIMBIM) + static_cast<long long>(BAMBAM);
+    if (result != BIMBIM + BAMBAM) {
+        return ERRORS::BAD_RESULT;
+    }
+    return result >> 32;
 }
